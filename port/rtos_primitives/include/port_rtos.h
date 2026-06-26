df0b798bbc2aa47ec54b9db09f80fa50be8a8e9c
EventHor1zon <3v3ntH0r1z0n@protonmail.com>
2026-06-26 02:26:50 +0100

    some basic port rtos sync primitive declarations
    now with docstrings!

--- port/rtos_primitives/include/port_rtos.h
+++ port/rtos_primitives/include/port_rtos.h
@@ -14,12 +14,68 @@
 #define PORT_RTOS_H
 
 /** Includes **/
+#include "port/error_types.h"
+#include "stdint.h"
 
 /** Defines **/
 
 /** Typedefs **/
 
+typedef void * locktype_t;
+typedef void * queuetype_t;
+typedef void * tasktype_t;
+
+
+
 /** Function Declarations **/
+
+/** @brief lock resource - take the semaphore 
+ *  @param lock [in] void pointer to lock handle
+ *  @param timeout [in] time to wait for operation
+ *  @return status_t status
+ */
+status_t port_lock_resource(locktype_t lock, uint32_t timeout);
+
+/** @brief unlock resource - give the semaphore 
+ *  @param lock [in] void pointer to lock handle
+ *  @return status_t status
+ */
+status_t port_unlock_resource(locktype_t lock);
+
+/** @brief get an item from the queue 
+ *         the user is responsible for correct item sizing
+ *  @param queue [in] void pointer to queue handle
+ *  @param buffer [in] pointer to data buffer
+ *  @param timeout [in] time to wait for operation
+ *  @return status_t status
+ */
+status_t port_queue_get(queuetype_t queue, uint8_t * const buffer, uint32_t timeout);
+
+/** @brief put an item into the queue 
+ *         the user is responsible for correct item sizing
+ *  @param queue [in] void pointer to queue handle
+ *  @param buffer [in] pointer to data
+ *  @param timeout [in] time to wait for operation
+ *  @return status_t status
+ */
+status_t port_queue_put(queuetype_t queue, const uint8_t *const buffer, uint32_t timeout);
+
+/** @brief get the current queue length
+ *  @param queue [in] void pointer to queue handle
+ *  @param len [out] length of the queue - 0 if error
+ *  @return status_t status
+ */
+status_t port_queue_get_len(queuetype_t queue, uint32_t * const len);
+
+/** @brief send a notification to a task
+ *  @param task [in] void pointer to task handle
+ *  @param notify_value [in] notification value to send
+ *  @param notify_type [in] type of notification to send
+ *  @return status_t status
+ */
+status_t port_task_notify(tasktype_t task, const uint32_t notify_value, const uint32_t notify_type);
+
+
 
 /** END **/
 #endif /** PORT_RTOS_H **/
