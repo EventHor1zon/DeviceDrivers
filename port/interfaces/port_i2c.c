e1dfbea1bc05f159f51ee837242824c57a26eea7
EventHor1zon <3v3ntH0r1z0n@protonmail.com>
2026-06-27 01:56:53 +0100

    move port files into sensible places

--- port/interfaces/port_i2c.c
+++ port/interfaces/port_i2c.c
@@ -1,17 +1,16 @@
 /**
-*    @file    port_i2c.c
-*
-*    @brief    header file for port_i2c
-*
-*
-*
-*    @author    RJAM
-*    @created   Fri 12 Jun 00:41:31 BST 2026
-*/
-
-
+ *    @file    port_i2c.c
+ *
+ *    @brief    source file for port_i2c
+ *
+ *
+ *
+ *    @author    RJAM
+ *    @created   Wed 24 Jun 00:00:00 BST 2026
+ */
 
 /** Includes **/
+#include "./include/port_i2c.h"
 
 /** Private Data **/
 
@@ -22,5 +21,69 @@
 /** Tasks **/
 
 /** Public Functions **/
+
+status_t __attribute__((weak))
+i2c_write(const uint32_t i2c_port, const uint32_t address, const uint8_t data)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak))
+i2c_read(const uint32_t i2c_port, const uint32_t address, uint8_t *const data)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak)) i2c_burst_write(
+    const uint32_t i2c_port,
+    const uint32_t address,
+    const uint8_t *const data,
+    const uint32_t len)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak)) i2c_burst_read(
+    const uint32_t i2c_port,
+    const uint32_t address,
+    uint8_t *const data,
+    const uint32_t len)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak)) i2c_register_write(
+    const uint32_t i2c_port,
+    const uint8_t ic_address,
+    const uint8_t reg,
+    uint32_t *const len,
+    const uint8_t *const data)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak)) i2c_register_read(
+    const uint32_t i2c_port,
+    const uint8_t ic_address,
+    const uint8_t reg,
+    uint32_t *const len,
+    uint8_t *const buffer)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak)) i2c_transact(
+    const uint32_t i2c_port,
+    const uint32_t address,
+    void *const transaction,
+    void *const args)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak)) i2c_init(const uint32_t i2c_port, const void *const args)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
 
 /** END **/
--- port/interfaces/port_spi.c
+++ port/interfaces/port_spi.c
@@ -1,17 +1,16 @@
 /**
-*    @file    port_spi.c
-*
-*    @brief    header file for port_spi
-*
-*
-*
-*    @author    RJAM
-*    @created   Fri 12 Jun 00:41:27 BST 2026
-*/
-
-
+ *    @file    port_spi.c
+ *
+ *    @brief    source file for port_spi
+ *
+ *
+ *
+ *    @author    RJAM
+ *    @created   Wed 24 Jun 00:00:00 BST 2026
+ */
 
 /** Includes **/
+#include "./include/port_spi.h"
 
 /** Private Data **/
 
@@ -22,5 +21,44 @@
 /** Tasks **/
 
 /** Public Functions **/
+
+status_t __attribute__((weak)) spi_write(const uint32_t spi_port, const uint8_t data)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak)) spi_read(const uint32_t spi_port, uint8_t *const data)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak))
+spi_transfer(const uint32_t spi_port, const uint8_t tx_data, uint8_t *const rx_data)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak))
+spi_transaction(const uint32_t spi_port, const void *const args, const uint32_t timeout)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak))
+spi_burst_write(const uint32_t spi_port, const uint8_t *const data, const uint32_t len)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak))
+spi_burst_read(const uint32_t spi_port, uint8_t *const data, const uint32_t len)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+status_t __attribute__((weak)) spi_init(const uint32_t spi_port, const void *const args)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
 
 /** END **/
